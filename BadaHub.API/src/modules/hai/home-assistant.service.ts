ae530725f03d74a3ff31de74695244edf45db034
Joe Badaczewski <badaczewskij@live.com>
2017-12-11 16:05:15 -0500

    troubleshooting and fixing issue where HA websocket would close immediately

--- BadaHub.API/src/modules/hai/home-assistant.service.ts
+++ BadaHub.API/src/modules/hai/home-assistant.service.ts
@@ -2,30 +2,32 @@ import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/map';
 import {Component} from '@nestjs/common';
 import {Event} from '../events/models/event';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Subject} from "rxjs/Subject";
+import {ReplaySubject} from "rxjs/ReplaySubject";
+
 let WebSocketClient = require('websocket').client;
 
 @Component()
 export class HomeAssistantService {
 
     private homeAssistantWebSocket: any;
-    private _hass: any;
-    private requestCount: number = 202;
-    private eventStream: BehaviorSubject<string> = new BehaviorSubject("");
+    private eventStream: Subject<any> = new ReplaySubject();
 
     constructor() {
         this.homeAssistantWebSocket = new WebSocketClient();
-        this.homeAssistantWebSocket.connect('ws://192.168.0.8:8123/api/websocket');
-
 
         this.homeAssistantWebSocket.on('connect', (connection) => {
-             this._hass = connection;
-             this.eventStream.subscribe(data => {
-                 return connection.sendUTF(data)
-             });
+
+            this.eventStream.subscribe(data => {
+                return connection.send(data)
+            });
+
+            connection.on('message', (message) => {
+                console.log(message);
+            });
         });
 
-
+        this.homeAssistantWebSocket.connect('ws://localhost:8123/api/websocket')
 
     }
 
@@ -34,13 +36,12 @@ export class HomeAssistantService {
     public triggerHomeAssistantEvent = (event: Event) => {
         //todo send web socket event to home assistant
         this.eventStream.next(JSON.stringify({
-            'id': 600,
-            'type': 'call_service',
-            'domain': 'light',
-            'service': 'turn_on',
-            'service_data': {'entity_id': 'light.bed_light'}
+            "id": event.id,
+            "type": "call_service",
+            "domain": "light",
+            "service": "turn_on",
+            "service_data": {"entity_id": "light.bed_light"}
         }));
-        console.log(event)
     }
 
 }
