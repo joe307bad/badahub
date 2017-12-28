fbd7023ca1ef1e84cf9fa4b61d8e82635cbdbb4f
Joe Badaczewski <badaczewskij@live.com>
2017-12-28 17:15:50 -0500

    trying to solve ProjectTo issue

--- BadaHub.API/BadaHub.API.Domain/Interfaces/IOperationRepository.cs
+++ BadaHub.API/BadaHub.API.Domain/Interfaces/IOperationRepository.cs
@@ -1,13 +1,15 @@
 ﻿using BadaHub.API.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BadaHub.API.Domain.Interfaces
 {
-    public interface IOperationRepository
+    public interface IOperationRepository : IRepository<Operation>
     {
         Operation GetById(Guid id);
+        //should this be iqueryable?
         IEnumerable<Operation> GetAll(); 
     }
 }
